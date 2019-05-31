60ad777d9c5f36b74385368fe6b6cc7dab53dd4b
Dan Stetsenko <denyast@ukr.net>
2019-06-01 01:50:27 +0300

    Minor styling adjustments.

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -4,7 +4,10 @@
     :to="{ name: 'movie-show', params: { id: movie.episode_id } }"
   >
     <div class="movie-card -shadow">
-      <h2>This is {{ movie.title }}'s card</h2>
+      <span class="movie-release-date">
+        {{ movie.release_date.substring(0, movie.release_date.indexOf('-')) }}
+      </span>
+      <h2>{{ movie.title }}</h2>
       <!-- <span class="eyebrow">@{{ movie.time }} on {{ movie.date }}</span> -->
       <!-- <h4 class="title">{{ movie.title }}</h4> -->
       <!-- <BaseIcon name="users">{{ movie.attendees.length }} attending</BaseIcon> -->
@@ -20,23 +23,30 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .movie-card {
-  padding: 10px 30px;
-  margin-bottom: 24px;
+  padding: 15px 30px;
+  margin-bottom: 15px;
   transition: all 0.2s linear;
   cursor: pointer;
-}
-.movie-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.2);
-}
-.movie-card > .title {
-  margin: 0;
+  &:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.2);
+  }
+  &.title {
+    margin: 0;
+  }
 }
 .movie-link {
   color: #2c3e50;
   text-decoration: none;
   font-weight: 100;
+}
+.movie-release-date {
+  padding-inline-start: 15px;
+}
+h2 {
+  margin-block-start: 5px;
+  margin-block-end: 0px;
 }
 </style>
